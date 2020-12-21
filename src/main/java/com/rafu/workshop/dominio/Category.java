b943e21628809d7adefae53c717c519a7ed945de
Rafael Henrique <fel.rique71@gmail.com>
2020-12-21 18:33:32 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/rafu/workshop/dominio/Category.java
+++ src/main/java/com/rafu/workshop/dominio/Category.java
@@ -22,6 +22,7 @@ public class Category implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
 	@JsonIgnore
 	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products =  new HashSet<>();
